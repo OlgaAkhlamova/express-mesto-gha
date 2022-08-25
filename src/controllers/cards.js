b928c5bb367a6d131bf443760a0653892b3e8198
Olga Akhlamova <olga-akhlamova@yandex.ru>
2022-08-26 01:08:55 +0300

    тест завис, что-то я не то сделала

--- src/controllers/cards.js
+++ src/controllers/cards.js
@@ -3,7 +3,20 @@ const Card = require('../models/card');
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.send({ message: `Произошла ошибка ${err.name} c текстом ${err.message}` }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: `Ошибка валидации: ${err.message}` });
+        return;
+      }
+      if (err.name === 'NotFound') {
+        res.status(404).send({ message: `Ресурс не найден: ${err.message}` });
+        return;
+      }
+      if (err.name === 'ServerError') {
+        res.status(500).send({ message: `Ошибка сервера: ${err.message}` });
+      }
+      console.log({ message: `Произошла неизвестная ошибка ${err.name} c текстом ${err.message}` });
+    });
 };
 
 module.exports.createCard = (req, res) => {
@@ -12,7 +25,20 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
   Card.create({ name, link, owner })
     .then((card) => res.status(201).send(card))
-    .catch((err) => res.send({ message: `Произошла ошибка ${err.name} c текстом ${err.message}` }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: `Ошибка валидации: ${err.message}` });
+        return;
+      }
+      if (err.name === 'NotFound') {
+        res.status(404).send({ message: `Ресурс не найден: ${err.message}` });
+        return;
+      }
+      if (err.name === 'ServerError') {
+        res.status(500).send({ message: `Ошибка сервера: ${err.message}` });
+      }
+      console.log({ message: `Произошла неизвестная ошибка ${err.name} c текстом ${err.message}` });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,22 +3,66 @@ const User = require('../models/user');
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.send({ message: `Произошла ошибка ${err.name} c текстом ${err.message}` }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: `Ошибка валидации: ${err.message}` });
+        return;
+      }
+      if (err.name === 'NotFound') {
+        res.status(404).send({ message: `Ресурс не найден: ${err.message}` });
+        return;
+      }
+      if (err.name === 'ServerError') {
+        res.status(500).send({ message: `Ошибка сервера: ${err.message}` });
+      }
+      console.log({ message: `Произошла неизвестная ошибка ${err.name} c текстом ${err.message}` });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.send({ message: `Произошла ошибка ${err.name} c текстом ${err.message}` }));
+  User.create({ name, about, avatar }, { runValidators: true })
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: `Ошибка валидации: ${err.message}` });
+        return;
+      }
+      if (err.name === 'NotFound') {
+        res.status(404).send({ message: `Ресурс не найден: ${err.message}` });
+        return;
+      }
+      if (err.name === 'ServerError') {
+        res.status(500).send({ message: `Ошибка сервера: ${err.message}` });
+      }
+      console.log({ message: `Произошла неизвестная ошибка ${err.name} c текстом ${err.message}` });
+    });
 };
 
 module.exports.getUser = (req, res) => {
-  const { userId } = req.params;
-  console.log(req.params);
-  User.findById(userId)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.send({ message: `Произошла ошибка ${err.name} c текстом ${err.message}` }));
+  console.log(req.params.userId);
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Ресурс не найден' });
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: `Ошибка валидации: ${err.message}` });
+        return;
+      }
+      if (err.name === 'NotFound') {
+        res.status(404).send({ message: `Ресурс не найден: ${err.message}` });
+        return;
+      }
+      if (err.name === 'ServerError') {
+        res.status(500).send({ message: `Ошибка сервера: ${err.message}` });
+        return;
+      }
+      console.log({ message: `Произошла неизвестная ошибка ${err.name} c текстом ${err.message}` });
+    });
 };
 
 module.exports.patchUser = (req, res) => {
@@ -28,7 +72,20 @@ module.exports.patchUser = (req, res) => {
     runValidators: true,
   })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.send({ message: `Произошла ошибка ${err.name} c текстом ${err.message}` }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: `Ошибка валидации: ${err.message}` });
+        return;
+      }
+      if (err.name === 'NotFound') {
+        res.status(404).send({ message: `Ресурс не найден: ${err.message}` });
+        return;
+      }
+      if (err.name === 'ServerError') {
+        res.status(500).send({ message: `Ошибка сервера: ${err.message}` });
+      }
+      console.log({ message: `Произошла неизвестная ошибка ${err.name} c текстом ${err.message}` });
+    });
 };
 
 module.exports.patchAvatar = (req, res) => {
@@ -37,6 +94,6 @@ module.exports.patchAvatar = (req, res) => {
     new: true,
     runValidators: true,
   })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send({ user }))
     .catch((err) => res.send({ message: `Произошла ошибка ${err.name} c текстом ${err.message}` }));
 };
