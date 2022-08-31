f74779732e8fb3523c262d1a9ef15087cdfea99f
Olga Akhlamova <olga-akhlamova@yandex.ru>
2022-08-31 09:51:07 +0300

    исправление ошибки  автотестов: несуществующий путь

--- app.js
+++ app.js
@@ -31,17 +31,16 @@ app.post('/signup', regValidation, createUser);
 
 app.use('/', auth, userRouter);
 app.use('/', auth, cardRouter);
+app.use('/', (req, res) => {
+  res.status(NOT_FOUND).send({ message: 'Страница не найдена' });
+});
 
 app.use(errors());
 
-app.use('/', (err, req, res, next) => {
-  if (err.name === 'NotFound') {
-    throw new NotFoundError('Страница не найдена');
-  } else {
-    const { statusCode = 500, message } = err;
-    res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
-    next();
-  }
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  next();
 });
 
 app.listen(PORT, () => {
